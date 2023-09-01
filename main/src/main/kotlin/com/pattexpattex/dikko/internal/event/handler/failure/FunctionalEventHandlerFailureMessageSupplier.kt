b0240f1a8d2c370c50ab7ef7bb9513bc2b34c12b
PattexPattex <bojan.brdar.turk@gmail.com>
2023-09-01 17:00:37 +0200

    Add FunctionalEventHandlerFailureMessageSupplier#default

--- main/src/main/kotlin/com/pattexpattex/dikko/internal/event/handler/failure/FunctionalEventHandlerFailureMessageSupplier.kt
+++ main/src/main/kotlin/com/pattexpattex/dikko/internal/event/handler/failure/FunctionalEventHandlerFailureMessageSupplier.kt
@@ -1,16 +1,19 @@
 package com.pattexpattex.dikko.internal.event.handler.failure
 
+import com.pattexpattex.dikko.api.event.handler.EventHandlerFailureMessageSupplier
 import com.pattexpattex.dikko.api.path.Path
 import net.dv8tion.jda.api.Permission
 
-class FunctionalEventHandlerFailureMessageSupplier : DefaultEventHandlerFailureMessageSupplier() {
-    var unknownError: () -> String = { super.unknownError() }
-    var noEventHandler: (Path) -> String = { super.noEventHandler(it) }
-    var noDefinition: (Path) -> String = { super.noDefinition(it) }
-    var noSelfPermissions: (List<Permission>) -> String = { super.noSelfPermissions(it) }
-    var noUserPermissions: (List<Permission>) -> String = { super.noUserPermissions(it) }
-    var actionUnavailable: (availableInGuild: Boolean, availableInDM: Boolean) -> String = { guild, dm ->  super.actionUnavailable(guild, dm) }
-    var invocationException: (Throwable) -> String = { super.invocationException(it) }
+class FunctionalEventHandlerFailureMessageSupplier : EventHandlerFailureMessageSupplier {
+    val default = DefaultEventHandlerFailureMessageSupplier()
+
+    var unknownError: () -> String = { default.unknownError() }
+    var noEventHandler: (Path) -> String = { default.noEventHandler(it) }
+    var noDefinition: (Path) -> String = { default.noDefinition(it) }
+    var noSelfPermissions: (List<Permission>) -> String = { default.noSelfPermissions(it) }
+    var noUserPermissions: (List<Permission>) -> String = { default.noUserPermissions(it) }
+    var actionUnavailable: (availableInGuild: Boolean, availableInDM: Boolean) -> String = { guild, dm ->  default.actionUnavailable(guild, dm) }
+    var invocationException: (Throwable) -> String = { default.invocationException(it) }
 
     override fun unknownError() = unknownError.invoke()
     override fun noEventHandler(path: Path) = noEventHandler.invoke(path)
